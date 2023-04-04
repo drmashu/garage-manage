b41eb961b004641b1fb1a219b6a967b078eb2b8a
NAGASAWA Takahiro <drmashu@gmail.com>
2023-04-04 23:13:13 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'garage-manage',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
