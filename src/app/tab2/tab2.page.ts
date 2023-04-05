2c87b354715ff6a2cf16a88a272943f6c015e9a5
NAGASAWA Takahiro <drmashu@gmail.com>
2023-04-05 12:38:23 +0900

    Tab2 → Settings

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,13 +3,13 @@ import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
+  selector: 'app-settings',
+  templateUrl: 'settings.page.html',
+  styleUrls: ['settings.page.scss'],
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent]
 })
-export class Tab2Page {
+export class SettingsPage {
 
   constructor() {}
 
