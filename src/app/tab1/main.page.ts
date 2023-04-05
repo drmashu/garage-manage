b73112cd86308316dd875b03364d3c19cffc7eeb
NAGASAWA Takahiro <drmashu@gmail.com>
2023-04-05 12:33:07 +0900

    Tab1→Main

--- src/app/tab1/main.page.ts
+++ src/app/tab1/main.page.ts
@@ -3,12 +3,12 @@ import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  selector: 'app-main',
+  templateUrl: 'main.page.html',
+  styleUrls: ['main.page.scss'],
   standalone: true,
   imports: [IonicModule, ExploreContainerComponent],
 })
-export class Tab1Page {
+export class MainPage {
   constructor() {}
 }
